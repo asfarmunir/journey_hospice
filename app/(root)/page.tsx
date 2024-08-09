eb45e95a319a68d0790d9e787f910babec4cba6e
Muhammad Shayan <54770763+SHAYAN9112K@users.noreply.github.com>
2024-08-09 23:23:11 +0500

    Update Hero section text.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -31,9 +31,9 @@ const page = () => {
               className=" w-[70px] 2xl:w-[100px]  "
             />
             <h1 className="text-2xl  md:text-4xl 2xl:text-5xl text-center mb-3 md:mb-0 md:text-start text-[#20265B] font-bold max-w-lg leading-snug ">
-              Journey Hospice Services,
+              Journey Hospice Services,Inc 
               <br />
-              Inc Hand in Hand <br /> through the journey
+              Hand in Hand <br /> through the journey
             </h1>
             <button className=" bg-white p-2.5 2xl:p-3 px-3.5 2xl:px-4 text-primary-50 w-fit text-base 2xl:text-lg font-semibold inline-flex items-center gap-5">
               Learn More
